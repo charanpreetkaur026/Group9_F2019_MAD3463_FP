983a75e008c91462b6d71d17e482bd3ccf619526
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 16:58:34 -0400

    [Camy] :FullTime class with setter and getter

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -1,11 +1,33 @@
 package com.lambton;
 
-public class Vehicle {
+public abstract class Vehicle extends Employee {
 
 
     private String make;
     private String color;
     private String plate;
 
+    public String getMake() {
+        return make;
+    }
 
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
 }
